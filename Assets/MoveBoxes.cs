c95d9e6aee973430e7ad31df2944112d55e036bd
DrMerlin <kotefare@gmail.com>
2016-10-11 14:47:39 -0600

    Initial Commit

---
+++ Assets/MoveBoxes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBoxes : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		MoveForward ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void MoveForward()
+	{
+		iTween.MoveAdd ((gameObject), iTween.Hash ("z", .5f, "islocal", true, "time", .5, "easetype", "linear", "oncomplete", "MoveBack", "onCompleteTarget", gameObject));
+	}
+
+	void MoveBack()
+	{
+		iTween.MoveAdd ((gameObject), iTween.Hash ("z", -.5f, "islocal", true, "time", .5, "easetype", "linear", "oncomplete", "MoveForward", "onCompleteTarget", gameObject));
+	}
+}
